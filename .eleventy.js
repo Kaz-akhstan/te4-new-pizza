c532972ee28e149f0fb1b3e8d60a097cce29d590
= <=>
2023-09-21 15:28:29 +0200

    Css funkar

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy(".src/style.css")
-    eleventyConfig.addPassthroughCopy(".src/menu.json")
+    eleventyConfig.addPassthroughCopy("src/css")
     return {
         dir: {
             input: "src",
